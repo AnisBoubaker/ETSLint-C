ce970cd465737e5c46276f84db8c75ac0efd4342
Anis Boubaker <anis@boubaker.ca>
2019-12-24 14:01:08 -0500

    Added consule arguments handling and multiple files linting.

--- Linter.py
+++ Linter.py
@@ -1,24 +1,38 @@
 import rules
 from pycparser import c_ast, parse_file
 import pycparser_fake_libc
-
+from os import walk
+import os.path
 
 class Linter(object):
 
-    def __init__(self, reporter):
+    __extensions = ["c", "cpp"]
+
+    def __init__(self, reporter, file_path):
         self.__reporter = reporter
         self.__rules_available = []
+        self.__file_path = file_path
+        self.__files_to_parse = []
         self.__get_all_rules()
+        self.__get_all_parsable_files()
 
-    def run_all_rules(self, filename):
+    def __get_all_parsable_files(self):
+        self.__files_to_parse.clear()
+
+        for (dirpath, _, filenames) in walk(self.__file_path):
+            self.__files_to_parse += [dirpath + "/" + file for file in filenames if
+                                      os.path.splitext(file)[1][1:] in self.__extensions]
+
+    def run_all_rules(self):
         # Note that cpp is used. Provide a path to your own cpp or
         # make sure one exists in PATH.
         fake_libc_arg = "-I" + pycparser_fake_libc.directory
 
-        ast = parse_file(filename, use_cpp=True, cpp_args=r"-I" + fake_libc_arg)
+        for file in self.__files_to_parse:
+            ast = parse_file(file, use_cpp=True, cpp_args=r"-I" + fake_libc_arg)
 
-        for rule in self.__rules_available:
-            rule.visit(ast)
+            for rule in self.__rules_available:
+                rule.visit(ast)
 
     def __get_all_rules(self):
         classes = []
--- etslint.py
+++ etslint.py
@@ -1,14 +1,38 @@
 import sys
 from Linter import Linter
-from reporters import JsonReporter
+from reporters import *
+import argparse
+
+
+extensions = ["c", "cpp"]
+
+
+def run_linter():
+    arg_parser = argparse.ArgumentParser()
+    arg_parser.add_argument("path", help="Path to the C file or a folder containing the C code to analyse.")
+    arg_parser.add_argument("-r", "--report", help="The type of report that will be generated. 'print' report type"
+                                                   "prints the report to stdout.",
+                            choices=["print", "json"], default="print")
+    arg_parser.add_argument("-o", "--output", help="File path where the report will be written (requires -r)")
+    args = arg_parser.parse_args()
+
+    if args.report == "print" and args.output:
+        arg_parser.error("A report type other than 'print' must be set (using -r) in order to use -o option.")
+
+    if args.report != "print" and not args.output:
+        arg_parser.error("Output file must be specified (with -o option) when using a non 'print' report.")
+
+    # Creates the reporter to be used by the Linter
+    if args.report == "json":
+        lint_reporter = JsonReporter(args.output)
+    else:
+        lint_reporter = PrintReporter()
+
+    lint = Linter(lint_reporter, args.path)
+    lint.run_all_rules()
+
+    lint_reporter.generate_report()
+
 
 if __name__ == "__main__":
-    if len(sys.argv) > 1:
-        filename = sys.argv[1]
-    else:
-        filename = 'examples/memmgr.c'
-
-    reporter = JsonReporter('report.json')
-    lint = Linter(reporter)
-    lint.run_all_rules(filename)
-    print(reporter.generate_report())
+    run_linter()
