ac06a39d259a68e367522b7e82a8b128ab52c409
Anis Boubaker <anis@boubaker.ca>
2019-12-27 00:26:37 -0500

    Added rule that detects expressions used as statement with no side effects.

--- clinter/rules/bugprone/RuleUnusedExpression.py
+++ clinter/rules/bugprone/RuleUnusedExpression.py
@@ -1,7 +1,11 @@
 from clinter.rules import BaseRule, BaseTest, RuleExecutionError
 from etslint import CLinter
+from pycparser.c_ast import Compound
 
 class RuleUnusedExpression(BaseRule):
+
+    __side_effect_ops = ['p++', 'p--', '+=', '-=', '*=', '%=', '<<=', '>>=', '&=', '^=', '|=']
+
     def __init__(self, reporter=None):
         BaseRule.__init__(self, "static", reporter)
         self.__parenter = None
@@ -9,23 +13,18 @@ class RuleUnusedExpression(BaseRule):
     def visit_UnaryOp(self, node):
         if self.__parenter is None:
             raise RuleExecutionError("Unable to access node parents.")
+        the_parent = self.__parenter.get_parent(node)
+        message = "Expression's result is not used."
+        if isinstance(the_parent, Compound) and node.op not in self.__side_effect_ops:
+            self.reporter.do_report(self, node.coord, message)
+
+    def visit_BinaryOp(self, node):
+        self.visit_UnaryOp(node)
 
 
     def visit_FileAST(self, node):
         self.__parenter = CLinter.NodeParenter.get_instance(node)
         self.generic_visit(node)
-
-
-    """def visit_Compound(self, node):
-        message = "Expression's result is not used."
-        for stmt in node.stmt.block_items:
-            if isinstance(stmt, UnaryOp) or isinstance(stmt, BinaryOp):
-                self.reporter.do_report(self, node.coord, message)
-            elif isinstance(stmt, Compound):
-                self.visit_Compound(stmt)
-            elif isinstance(stmt, For):
-                self.visit_Compound(stmt)
-    """
 
 
 class TestUnusedExpression(BaseTest):
@@ -34,16 +33,70 @@ class TestUnusedExpression(BaseTest):
 
     def test1(self):
         self._tested_code = """
+
+        int main();
+
+        int a;
+
+        int main(){
+            int a = 10 ;
+
+            for( ; a -20 ; ){
+                printf("");
+
+            }
+        }
+        """
+        self._run_rule()
+        self.expect_no_error("Expression used as condition OK")
+
+    def test2(self):
+        self._tested_code = """
+
+        int main();
+
+        int main(){
+            int a = 10 ;
+            
+            a - 20;
+            
+            for( ; a < 10 ; ){
+                a + 20;
+
+            }
+        }
+        """
+        self._run_rule()
+        self.expect_error("Useless expression in block NOOK")
+
+    def test3(self):
+        self._tested_code = """
         
         int main();
         
         int main(){
             int a = 10 ;
             for( ; a < 10 ; ){
-
+                a + 20;
 
             }
         }
         """
         self._run_rule()
-        #self.expect_no_error("For loop with empty body NOOK")
+        self.expect_error("Useless expression in internal block NOOK")
+
+    def test4(self):
+        self._tested_code = """
+
+        int main();
+
+        int main(){
+            int a = 10 ;
+            for( ; a < 10 ; ){
+                a &= 5;
+
+            }
+        }
+        """
+        self._run_rule()
+        self.expect_no_error("Expression using side effect operator OK")
