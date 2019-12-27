1cdce1f325d19031953915b5251d877bc457a54e
Anis Boubaker <anis@boubaker.ca>
2019-12-27 00:32:08 -0500

    Added the printing of the file name in the Print reporter.

--- clinter/reporters/PrintReporter.py
+++ clinter/reporters/PrintReporter.py
@@ -7,7 +7,10 @@ class PrintReporter(BaseReporter):
 
     def generate_report(self):
         reports = self.reports
+        current_file_name = None
         for report in reports:
+            if current_file_name is None or report.file_name != current_file_name:
+                current_file_name = report.file_name
+                print("**** Reports for file: " + current_file_name)
             print("{}:{}:{} {} [{}]".format(report.file_name, report.line, report.column,
                                             report.message, str(report.rule)))
-
